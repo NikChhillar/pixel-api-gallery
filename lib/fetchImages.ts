93e9b3116beb716c0916d3ef406aa7828f8a1692
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-05 11:12:47 +0530

    fetch images 🔗

---
+++ lib/fetchImages.ts
@@ -0,0 +1,28 @@
+import type { ImagesResults } from "@/models/Images";
+import { ImagesSchema } from "@/models/Images";
+import env from "./env";
+
+export default async function fetchImages(
+  url: string
+): Promise<ImagesResults | undefined> {
+  try {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: env.PEXELS_API_KEY,
+      },
+    });
+
+    if (!res.ok) throw new Error("Falied to fetch images....");
+
+    const imagesResult: ImagesResults = await res.json();
+    console.log(imagesResult);
+
+    const parsedData = ImagesSchema.parse(imagesResult);
+
+    if (parsedData.total_results === 0) return undefined;
+
+    return parsedData;
+  } catch (e) {
+    if (e instanceof Error) console.log(e.stack);
+  }
+}
