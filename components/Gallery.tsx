8666b4b411b7a81c4d77387bd9562221d1f8b521
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-08 12:29:30 +0530

    pagination function 🔗

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -2,29 +2,44 @@ import fetchImages from "@/lib/fetchImages";
 import { ImagesResults } from "@/models/Images";
 import ImageCon from "./ImageCon";
 import addBlurredDataUrls from "@/lib/getBase64";
+import Footer from "./Footer";
 
 type Props = {
   topic?: string | undefined;
+  page?: string | undefined;
 };
 
-const Gallery = async ({ topic }: Props) => {
-  const url = !topic
-    ? "https://api.pexels.com/v1/curated"
-    : `https://api.pexels.com/v1/search?query=${topic}`;
+const Gallery = async ({ topic = "curated", page }: Props) => {
+  //
+
+  let url;
+
+  if (topic === "curated" && page) {
+    url = `https://api.pexels.com/v1/curated?page=${page}`;
+  } else if (topic === "curated") {
+    url = "https://api.pexels.com/v1/curated";
+  } else if (!page) {
+    url = `https://api.pexels.com/v1/search?query=${topic}`;
+  } else {
+    url = `https://api.pexels.com/v1/search?query=${topic}&page=${page}`;
+  }
 
   const images: ImagesResults | undefined = await fetchImages(url);
 
-  if (!images)
+  if (!images || images.per_page === 0)
     return <h2 className="m-4 text-2xl font-bold">No images found...</h2>;
 
   const photoWithBlur = await addBlurredDataUrls(images);
 
   return (
-    <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
-      {photoWithBlur.map((photo) => (
-        <ImageCon key={photo.id} photo={photo} />
-      ))}
-    </section>
+    <>
+      <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
+        {photoWithBlur.map((photo) => (
+          <ImageCon key={photo.id} photo={photo} />
+        ))}
+      </section>
+      <Footer />
+    </>
   );
 };
 
